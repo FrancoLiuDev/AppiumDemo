dd3d6b253180cceb2b578477b57228afb4d97b71
Franco Liu <francoliu@Francos-Mac-mini.local>
2016-10-07 16:47:30 +0800

    AppiumDemo

---
+++ AppiumTestProject/Login/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  AppiumTestProject
+//
+//  Created by Franco Liu on 07/10/2016.
+//  Copyright © 2016 Franco Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
